2fe4c53a357805176f409855f761b57f792d6e5b
piyush kumar sarangi <105096649+sarangipiyush7@users.noreply.github.com>
2022-07-24 01:13:31 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "piyush am i";
+        return "HARI HARA";
     }
 
 }
